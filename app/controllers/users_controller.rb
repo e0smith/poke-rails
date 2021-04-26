d7241c59fed3ecf71eb54d7856f45bb5e25d10f0
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-04-26 11:23:51 -0400

    fixed new user not saved to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            render :'welcome/home'
+        else
+            render :new
+        end
     end
 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password_digest)
+    end
 end
