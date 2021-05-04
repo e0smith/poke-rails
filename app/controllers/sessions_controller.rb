d99f761f73749954c03ce8c11ed4f30ff01a6b55
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-04 18:34:52 -0400

    google signin fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,9 +27,11 @@ class SessionsController < ApplicationController
       @user = User.find_or_create_by(uid: auth['uid']) do |u|
         u.name = auth[:info][:name]
         u.email = auth[:info][:email]
+        u.username = auth[:info][:name]
         u.uid = auth[:uid]
         u.provider = auth[:provider]
         u.password = SecureRandom.hex(10)
+        u.save
       end
 
       session[:user_id] = @user.id
